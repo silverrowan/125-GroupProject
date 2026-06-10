0933d80b1240db29f8316b219346b1d6e889fbf7
Max Zhang <max.z.e.zhang2007@gmail.com>
2026-06-10 15:46:51 +0000

    Setup initial project structure and libraries
    Merge branch 'dev' into 'master'
    See merge request 128-seng-group-project/125-travel_booking_app!2
    
    SPECIFICS
    - Added MySQL connector JAR Library files
    - Setup MVC folders (w/ dummy files so structure uploads properly)

---
+++ Travel_Booking_App/src/controller/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author c0541856
+ */
+public class NewClass {
+    
+}
