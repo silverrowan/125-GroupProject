f6a9a04a4ce65d33688b65b767b77f6b1c3a8a91
c0541856 <c0541856@TB148-02.intra.camosun.bc.ca>
2026-06-12 12:33:50 -0700

    Created Customer class

---
+++ Travel_Booking_App/src/model/Customer.java
@@ -0,0 +1,70 @@
+
+package model;
+
+/**
+ *
+ * @author Max Zhang
+ */
+public class Customer {
+    private int customerID;
+    private int userID;
+    private String emergencyContactName;
+    private String emergencyContactPhone;
+    private String customerNotes;
+
+    // full constructor
+    public Customer(int customerID, int userID, String emergencyContactName, String emergencyContactPhone, String customerNotes) {
+        this(customerID, userID);
+        this.emergencyContactName = emergencyContactName;
+        this.emergencyContactPhone = emergencyContactPhone;
+        this.customerNotes = customerNotes;
+    }
+
+    // required constructor
+    public Customer(int customerID, int userID) {
+        this.customerID = customerID;
+        this.userID = userID;
+    }
+    
+    // getters and setters
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getEmergencyContactName() {
+        return emergencyContactName;
+    }
+
+    public void setEmergencyContactName(String emergencyContactName) {
+        this.emergencyContactName = emergencyContactName;
+    }
+
+    public String getEmergencyContactPhone() {
+        return emergencyContactPhone;
+    }
+
+    public void setEmergencyContactPhone(String emergencyContactPhone) {
+        this.emergencyContactPhone = emergencyContactPhone;
+    }
+
+    public String getCustomerNotes() {
+        return customerNotes;
+    }
+
+    public void setCustomerNotes(String customerNotes) {
+        this.customerNotes = customerNotes;
+    }
+
+}
