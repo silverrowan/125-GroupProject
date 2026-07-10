b5d93ecc61891197115e4a5dd423d6e8e2056b9c
Rowan <silverrowan@gmail.com>
2026-07-09 21:48:21 -0700

    Create UserDAO with createNewUser()

---
+++ Travel_Booking_App/src/dao/UserDAO.java
@@ -0,0 +1,38 @@
+package dao;
+
+import model.User;
+
+import utility.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author rowan
+ */
+public class UserDAO {
+    public boolean createNewUser(User user) {
+        String query = "INSERT INTO users (username, password, first_name, last_name, email, role) VALUES (?,?,?,?,?,?);";
+        
+        try ( Connection link = DBConnection.getConnnection(); 
+            PreparedStatement p = link.prepareStatement(query) ) 
+        {
+            p.setString( 1, user.getUsername() );
+            p.setString( 2, user.getPassword() );
+            p.setString( 3, user.getFirstName() );
+            p.setString( 4, user.getLastName() );
+            p.setString( 5, user.getEmail() );
+            p.setString( 6, user.getRole() );
+            
+            int row = p.executeUpdate();
+            
+            return row > 0; //equiv to if true return true.
+        }
+        catch ( SQLException e ) {
+            e.getStackTrace();
+            return false;
+        }
+    }
+}
