9f90202b86f296820e425479dab51dbb2012053d
Mariah Malczewska <mariahmalcz@gmail.com>
2026-07-09 18:39:16 -0700

    Setup WIP controller for Destinations

---
+++ Travel_Booking_App/src/controller/DestinationController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import dao.DestinationsDao;
+import model.Destinations;
+import view.ViewDestinationsGUI;
+
+/**
+ *
+ * @author Mariah Malczewska
+ */
+public class DestinationController {
+    private final DestinationsDao dao;
+    private final ViewDestinationsGUI view;
+    
+    public DestinationController( DestinationsDao dao, ViewDestinationsGUI view ) {
+        this.dao = dao;
+        this.view = view;
+        
+        this.view.
+        })
+    }
+}
