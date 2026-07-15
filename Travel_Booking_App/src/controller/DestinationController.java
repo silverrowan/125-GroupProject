79b66f3c000398f373e0621f7588e96af59d499e
Mariah Malczewska <mariahmalcz@gmail.com>
2026-07-14 20:00:46 -0700

    Fix name of DestinationDAO in Controller

--- Travel_Booking_App/src/controller/DestinationController.java
+++ Travel_Booking_App/src/controller/DestinationController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import dao.DestinationsDao;
+import dao.DestinationsDAO;
 import model.Destinations;
 import view.ViewDestinationsGUI;
 
@@ -9,14 +9,13 @@ import view.ViewDestinationsGUI;
  * @author Mariah Malczewska
  */
 public class DestinationController {
-    private final DestinationsDao dao;
+    private final DestinationsDAO dao;
     private final ViewDestinationsGUI view;
     
-    public DestinationController( DestinationsDao dao, ViewDestinationsGUI view ) {
+    public DestinationController( DestinationsDAO dao, ViewDestinationsGUI view ) {
         this.dao = dao;
         this.view = view;
         
-//        this.view.
-//        })
+
     }
 }
