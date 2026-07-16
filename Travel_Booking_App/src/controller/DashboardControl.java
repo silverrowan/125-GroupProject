0530d52c846e5e3ed92dec38d294589002a3d308
Mariah Malczewska <mariahmalcz@gmail.com>
2026-07-16 16:19:04 -0700

    Refactor MenuItems to Cards

--- Travel_Booking_App/src/controller/DashboardControl.java
+++ Travel_Booking_App/src/controller/DashboardControl.java
@@ -7,10 +7,11 @@ import java.nio.file.AccessDeniedException;
 import java.util.LinkedList;
 import javax.naming.InvalidNameException;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import model.User;
-import view.models.Model_MenuItem;
+import view.models.Card;
 import utility.Session;
 import view.AddUserGUIPage1;
 import view.Login;
@@ -18,7 +19,8 @@ import view.ProductsGUI;
 import view.ViewCustomerGUI;
 import utility.AppWindow;
 import view.components.ListMenu;
-import view.DashboardMenu;
+import view.dashboard.DashboardMenu;
+import view.models.ImgCard;
 
 /**
  *
@@ -32,60 +34,60 @@ public class DashboardControl<T> {
 //    private DashboardMenu menu;
     private ListMenu menuA;
     private ListMenu menuB;
-    private LinkedList<Model_MenuItem> menuList = new LinkedList<Model_MenuItem>();
+    private LinkedList<Card> menuList = new LinkedList<Card>();
     
 //    public DashboardControl(){}
 //    public DashboardControl( AppContext context, DashboardMenu menu ){
     public DashboardControl( AppContext context, AppWindow dashWindow ){
         this.context = context;
         this.dashWindow = dashWindow;
-        this.menuA = dashWindow.getDashboardList().getMenuListA();
-        this.menuB = dashWindow.getDashboardList().getMenuListB();
+//        this.menuA = dashWindow.getDashboardList().getMenuListA();
+//        this.menuB = dashWindow.getDashboardList().getMenuListB();
 }
     
     public void initialize() { 
-        menuA.addListSelectionListener( new MenuSelect() ); //menu listener
-        menuB.addListSelectionListener( new MenuSelect() ); //menu listener
+//        menuA.addListSelectionListener( new MenuSelect() ); //menu listener
+//        menuB.addListSelectionListener( new MenuSelect() ); //menu listener
         buildMenu(); 
     }
         
     private void buildMenu(){
         User currentUser = context.getCurrentSession().getCurrentUser();
-                addToMenuList(new Model_MenuItem("", "Welcome " + currentUser.getUsername(), Model_MenuItem.MenuType.HEADER, ""));
+                addToMenuList(new Card("Welcome " + currentUser.getUsername(), Card.CardType.HEADER, ""));
         switch ( currentUser.getRole() ) {
             case "Admin":
                 
-                addToMenuList(new Model_MenuItem("id-card", "My Profile", Model_MenuItem.MenuType.MENU, "ViewAdminProfile"));
+                addToMenuList(new ImgCard("id-card", "My Profile", Card.CardType.MENU, "ViewAdminProfile"));
                 
-                addToMenuList(new Model_MenuItem("", "", Model_MenuItem.MenuType.SEPARATOR, ""));
+                addToMenuList(new Card( "", Card.CardType.SEPARATOR, ""));
                 
-                addToMenuList(new Model_MenuItem("add_location_alt", "Search Products", Model_MenuItem.MenuType.MENU, "SearchProducts"));
-                addToMenuList(new Model_MenuItem("add_location_alt", "Search Packages", Model_MenuItem.MenuType.MENU, "SearchPackages"));
-                addToMenuList(new Model_MenuItem("add_location_alt", "Search Destinations", Model_MenuItem.MenuType.MENU, "SearchDestinations"));
+                addToMenuList(new ImgCard("add_location_alt", "Search Products", Card.CardType.MENU, "SearchProducts"));
+                addToMenuList(new ImgCard("add_location_alt", "Search Packages", Card.CardType.MENU, "SearchPackages"));
+                addToMenuList(new ImgCard("add_location_alt", "Search Destinations", Card.CardType.MENU, "SearchDestinations"));
                 
-                addToMenuList(new Model_MenuItem("", "", Model_MenuItem.MenuType.SEPARATOR, ""));
+                addToMenuList(new Card( "", Card.CardType.SEPARATOR, ""));
                 
-                addToMenuList(new Model_MenuItem("user-search-line", "Search Users", Model_MenuItem.MenuType.MENU, "SearchForUser"));
-                addToMenuList(new Model_MenuItem("user-search-line", "Search Bookings", Model_MenuItem.MenuType.MENU, "SearchForBooking"));
+                addToMenuList(new ImgCard("user-search-line", "Search Users", Card.CardType.MENU, "SearchForUser"));
+                addToMenuList(new ImgCard("user-search-line", "Search Bookings", Card.CardType.MENU, "SearchForBooking"));
                 try { addAdminTargetItems(); }
                 catch ( Exception e ) { System.out.println("exception " + e ); }
                 
-                addToMenuList(new Model_MenuItem("", "", Model_MenuItem.MenuType.SEPARATOR, ""));
+                addToMenuList(new Card( "", Card.CardType.SEPARATOR, ""));
                 
                 buildSharedMenu();
                 break;
             case "Travel Agent":
         //        Agent Name Header
-                addToMenuList(new Model_MenuItem("id-card", "My Profile", Model_MenuItem.MenuType.MENU, "ViewEmployeeProfile"));
-                addToMenuList(new Model_MenuItem("user-search-line", "Search Customers", Model_MenuItem.MenuType.MENU, "SearchForCustomer"));
+                addToMenuList(new ImgCard("id-card", "My Profile", Card.CardType.MENU, "ViewEmployeeProfile"));
+                addToMenuList(new ImgCard("user-search-line", "Search Customers", Card.CardType.MENU, "SearchForCustomer"));
         //        Active Customer Name Header
-                addToMenuList(new Model_MenuItem("id-card", "Customer Profile", Model_MenuItem.MenuType.MENU, "ViewCustomerProfile"));
-//                addToMenuList(new Model_MenuItem("id-card", "Customer Profile", Model_MenuItem.MenuType.MENU, "ViewCustomerProfile"));
+                addToMenuList(new ImgCard("id-card", "Customer Profile", Card.CardType.MENU, "ViewCustomerProfile"));
+//                addToMenuList(new Card("id-card", "Customer Profile", Card.CardType.MENU, "ViewCustomerProfile"));
                 buildSharedMenu();
                 break;
 //            case "Tour Guide":                
             case "Customer":
-                addToMenuList(new Model_MenuItem("id-card", "My Profile", Model_MenuItem.MenuType.MENU, "ViewCustomerProfile"));
+                addToMenuList(new ImgCard("id-card", "My Profile", Card.CardType.MENU, "ViewCustomerProfile"));
                 buildSharedMenu();
                 break;
             }
@@ -97,10 +99,10 @@ public class DashboardControl<T> {
         //refresh menu
     }
     
-    private void addToMenuList( Model_MenuItem item ){ menuList.addLast( item ); }
+    private void addToMenuList( Card item ){ menuList.addLast( item ); }
 
-    private void addToMenuList( String icon, String title, Model_MenuItem.MenuType type, String action ){
-        menuList.addLast( new Model_MenuItem(icon, title, type, action) );
+    private void addToMenuList( String icon, String title, Card.CardType type, String action ){
+        menuList.addLast( new ImgCard(icon, title, type, action) );
     }
     
     private void splitMenuLists() {
@@ -118,54 +120,54 @@ public class DashboardControl<T> {
     }
     
     private void buildSharedMenu(){
-        addToMenuList(new Model_MenuItem("world-search", "Packages", Model_MenuItem.MenuType.MENU, "SearchPackages"));
-        addToMenuList(new Model_MenuItem("calendar_plus", "Latest", Model_MenuItem.MenuType.MENU, "LastBooking"));
-//        addToMenuList(new Model_MenuItem("plans", "Plans", Model_MenuItem.MenuType.MENU, "ViewFutureBookingsOrItinerary"));
-        addToMenuList(new Model_MenuItem("calendar_clock", "History", Model_MenuItem.MenuType.MENU, "ViewBookings"));
-        addToMenuList(new Model_MenuItem("logout-box", "Logout", Model_MenuItem.MenuType.MENU, "Logout"));
+        addToMenuList(new ImgCard("world-search", "Packages", Card.CardType.MENU, "SearchPackages"));
+        addToMenuList(new ImgCard("calendar_plus", "Latest", Card.CardType.MENU, "LastBooking"));
+//        addToMenuList(new Card("plans", "Plans", Card.CardType.MENU, "ViewFutureBookingsOrItinerary"));
+        addToMenuList(new ImgCard("calendar_clock", "History", Card.CardType.MENU, "ViewBookings"));
+        addToMenuList(new ImgCard("logout-box", "Logout", Card.CardType.MENU, "Logout"));
     }
     
-    class MenuSelect implements ListSelectionListener {
+//    class MenuSelect implements ListSelectionListener {
 
-        @Override
-        public void valueChanged(ListSelectionEvent e) {
-            if (e.getValueIsAdjusting()) { return; }                
-            
-            Model_MenuItem itemA = menuA.getSelectedValue();
-            Model_MenuItem itemB = menuB.getSelectedValue();
-            Model_MenuItem item;
-            
-            if (itemA == null && itemB == null) { return; }
-            else if ( itemA != null ) { item = itemA; }
-            else { item = itemB; }
-
-            switch ( item.getLinkAction() ) {
-                case "Logout":
-                    logoutUser( context );
-                    break;
-                case "ViewBookings":
-                    makeViewCustomerBookings( context );
-                    break;
-                case "LastBooking":
-                    makeViewCustomerLastBooking( context );
-                    break;
-                case "SearchPackages":
-                    makeViewPackageSearch( context );
-                    break;
-                case "ViewCustomerProfile":
-                    makeViewProfile( context, false ); 
-                    break;
-                case "ViewEmployeeProfile":
-                    makeViewProfile( context, true ); 
-                    break;
-                default:
-                    System.out.println("Selection " + item.getLinkAction() + " has no valid target");
-                    break;
-            }
-            menuA.clearSelection(); // clears selection after completing - makes act like a button instead of a selection list
-            menuB.clearSelection(); // clears selection after completing - makes act like a button instead of a selection list
-        }
-    }
+//        @Override
+//        public void valueChanged(ListSelectionEvent e) {
+//            if (e.getValueIsAdjusting()) { return; }                
+//            
+//            Card itemA = menuA.getSelectedValue();
+//            Card itemB = menuB.getSelectedValue();
+//            Card item;
+//            
+//            if (itemA == null && itemB == null) { return; }
+//            else if ( itemA != null ) { item = itemA; }
+//            else { item = itemB; }
+//
+//            switch ( item.getTag()) {
+//                case "Logout":
+//                    logoutUser( context );
+//                    break;
+//                case "ViewBookings":
+//                    makeViewCustomerBookings( context );
+//                    break;
+//                case "LastBooking":
+//                    makeViewCustomerLastBooking( context );
+//                    break;
+//                case "SearchPackages":
+//                    makeViewPackageSearch( context );
+//                    break;
+//                case "ViewCustomerProfile":
+//                    makeViewProfile( context, false ); 
+//                    break;
+//                case "ViewEmployeeProfile":
+//                    makeViewProfile( context, true ); 
+//                    break;
+//                default:
+//                    System.out.println("Selection " + item.getTag()+ " has no valid target");
+//                    break;
+//            }
+//            menuA.clearSelection(); // clears selection after completing - makes act like a button instead of a selection list
+//            menuB.clearSelection(); // clears selection after completing - makes act like a button instead of a selection list
+//        }
+//    }
     
     private void addAdminTargetItems() throws AccessDeniedException, DuplicateTargetException{
         User loggedIn = context.getCurrentSession().getCurrentUser();
@@ -178,8 +180,8 @@ public class DashboardControl<T> {
             throw new DuplicateTargetException("Cannot have two active targets. Clearing both. Retry");
         } 
         else if ( employee != null ) {
-            addToMenuList( "", "Active Employee: " + employee.getUsername(), Model_MenuItem.MenuType.HEADER, "" );
-            addToMenuList(new Model_MenuItem("id-card", "Employee Profile", Model_MenuItem.MenuType.MENU, "ViewEmployeeProfile"));
+//            addToMenuList( new JLabel( "Active Employee: " + employee.getUsername(), Card.CardType.HEADER, "" ));
+            addToMenuList(new ImgCard("id-card", "Employee Profile", Card.CardType.MENU, "ViewEmployeeProfile"));
         } 
         else { addAgentTargetItems( customer ); }
 
@@ -188,12 +190,12 @@ public class DashboardControl<T> {
     private void addAgentTargetItems( User customer ) {
         if ( customer != null ) { 
             //use customer option
-            addToMenuList( "", "Active Customer: " + customer.getUsername(), Model_MenuItem.MenuType.HEADER, "" );
-            addToMenuList(new Model_MenuItem("id-card", "Customer Profile", Model_MenuItem.MenuType.MENU, "ViewCustomerProfile"));
+//            addToMenuList( "", "Active Customer: " + customer.getUsername(), Card.CardType.HEADER, "" );
+            addToMenuList(new ImgCard("id-card", "Customer Profile", Card.CardType.MENU, "ViewCustomerProfile"));
         } else {
             // NEW user when cleared
-            addToMenuList( "", "No Active Customer", Model_MenuItem.MenuType.HEADER, "" );
-            addToMenuList(new Model_MenuItem("id-card", "New User", Model_MenuItem.MenuType.MENU, "New User"));
+//            addToMenuList( "", "No Active Customer", Card.CardType.HEADER, "" );
+            addToMenuList(new ImgCard("id-card", "New User", Card.CardType.MENU, "New User"));
         }   
     }
     
