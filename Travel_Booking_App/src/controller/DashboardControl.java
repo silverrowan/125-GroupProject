1942eaf277a9801bcd7a66a03767867892769119
Mariah Malczewska <mariahmalcz@gmail.com>
2026-07-21 16:34:30 -0700

    minor refactoring; ClearCust actionPerformed() in DashboardControl

--- Travel_Booking_App/src/controller/DashboardControl.java
+++ Travel_Booking_App/src/controller/DashboardControl.java
@@ -176,9 +176,8 @@ public class DashboardControl extends GenericControl{
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                context.getCurrentSession().setCurrentCustomer( null );
+                context.getCurrentSession().clearCurrentCustomer();
                 refresh();
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
             }
         }
 
